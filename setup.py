69e9fdda0ff0a649e62085049e5ad9a14e9b60c1
yashguptatech <yashgupta.tech@gmail.com>
2023-07-14 04:32:27 +0530

    requirements added and setup done

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
-import os
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPONAME = "CNN-Classifier"
+AUTHOR_USER_NAME = "yashguptatech"
+SRC_REPO = "CNN-Classifier"
+AUTHOR_EMAIL = "yashgupta.tech@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+        
+)
